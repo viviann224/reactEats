bbcde49a2b00cfac1e849ffff3beb76ad208e17b
viviann224 <viviann224@gmail.com>
2019-03-19 12:10:07 -0500

    updated Navbar

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = () => (
-  <div className="jumbotron text-center">
-    <h1>React Recipes</h1>
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="http://www.recipepuppy.com/about/api/"
-    >
-      Powered by Recipe Puppy
-    </a>
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">React Recipes</h1>
+      <p className="lead"><a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.recipepuppy.com/about/api/"
+      >
+        Powered by Recipe Puppy
+      </a></p>
+    </div>
   </div>
 );
 
